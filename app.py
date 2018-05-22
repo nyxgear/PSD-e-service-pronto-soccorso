6929c8df947673af1512a579b3c3a3a1e5e53c4b
nyxgear <dev@nyxgear.com>
2018-05-22 15:15:06 +0200

    post request test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json, jsonify
 
 app = Flask(__name__)
 
@@ -17,3 +17,12 @@ def hello():
 def show_post(post_id):
 	# show the post with the given id, the id is an integer
 	return 'Post %d' % post_id
+
+
+@app.route('/requests/<int:request_id>', methods=['POST'])
+def post_requests(request_id):
+	body_request = json.loads(request.data)
+
+	print ('ANTONELLLA MANNAGGGGGGGIA')
+
+	return jsonify(body_request)
