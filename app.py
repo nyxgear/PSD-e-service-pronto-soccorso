34675f72ea0d53e4936cb274fc4eacae19fafd03
nyxgear <dev@nyxgear.com>
2018-05-22 14:09:24 +0200

    aggiunto script di run

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+	return 'Hello, World'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	# show the post with the given id, the id is an integer
+	return 'Post %d' % post_id
---
+++ run-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export FLASK_APP=app.py
+
+export FLASK_ENV=development
+
+flask run
