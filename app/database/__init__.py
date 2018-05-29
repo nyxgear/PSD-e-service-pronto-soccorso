ed9b7a8d806e120108124646a1c600291016006b
nyxgear <dev@nyxgear.com>
2018-05-29 14:38:21 +0200

    improved database functions

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
 
+
 def get(entity_class, attribute_name, attribute_value):
 	"""
 	Retrieve an instantiated entity from the database
@@ -8,19 +9,32 @@ def get(entity_class, attribute_name, attribute_value):
 	:param attribute_value: The attribute value to look for
 	:return:
 	"""
+	l = get_list(entity_class, attribute_name, attribute_value)
+
+	if len(l) > 0:
+		return l[0]
+
+	return None
+
+
+def get_list(entity_class, attribute_name=None, attribute_value=None):
+	"""
+	Retrieve a list of instantiated entities from the database
+	:param entity_class: Class of the entity e.g. db.EntityClass
+	:param attribute_name: The attribute name on which to query
+	:param attribute_value: The attribute value to look for
+	:return:
+	"""
 	res = []
 
-	# search for the entity
 	for x in entity_class._table:
-		value = x.get(attribute_name)
 
-		if value == attribute_value:
-			res.append(x)
+		if attribute_name is None and attribute_value is None:
+			res.append(entity_class(x))
+		else:
+			value = x.get(attribute_name)
 
-	# no results
-	if not res:
-		return None
+			if value == attribute_value:
+				res.append(entity_class(x))
 
-	x_dict = res[0]
-
-	return entity_class(x_dict)
+	return res
