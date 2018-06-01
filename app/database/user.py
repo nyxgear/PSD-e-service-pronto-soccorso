a2e23f364be474db3519151df97a93e4476a01a9
nyxgear <dev@nyxgear.com>
2018-06-01 12:46:13 +0200

    user entity updated

--- app/database/user.py
+++ app/database/user.py
@@ -8,14 +8,13 @@ from hashlib import sha512
 class User:
 	_table = table
 
-	def __init__(self, user_dict):
-		self.id_ = user_dict.get('id_')
-		self.email = user_dict.get('email')
-		self.salt = user_dict.get('salt')
-		self.password = user_dict.get('password')
+	def __init__(self, user_dict={}):
+		# entity dict
+		self.e_d = user_dict
+		self.password = self.e_d.pop('password', "no_password_set..")
 
 	def __repr__(self):
-		return 'User<id={}, email={}>'.format(self.id_, self.email)
+		return 'User<id={}, email={}>'.format(self.e_d.get('id'), self.e_d.get('email'))
 
 	def is_authenticated(self):
 		return False
@@ -27,27 +26,35 @@ class User:
 		return False
 
 	def get_id(self):
-		return self.id_
+		return self.e_d.get('id')
 
 	@property
 	def password(self):
+		# let's do not return the password :)
 		return None
 
 	@password.setter
 	def password(self, v):
+		if not self.e_d.get('salt'):
+			raise Exception('Salt for this user is lacking!', self)
+
 		hash = sha512()
-		hash.update(('-%s-%s-' % (self.salt, v)).encode('utf-8'))
+		hash.update(('-%s-%s-' % (self.e_d.get('salt'), v)).encode('utf-8'))
 		self._password = hash.hexdigest()
 
 	def is_valid_password(self, check_password):
 		""""
 		Check if the provided password is valid for the user.
 		"""
-		if not self.salt:
+		if not self.e_d.get('salt'):
 			return False
 
 		ck_hash = sha512()
-		ck_hash.update(('-%s-%s-' % (self.salt, check_password)).encode('utf-8'))
+		ck_hash.update(
+			('-%s-%s-' % (self.e_d.get('salt'), check_password)).encode('utf-8'))
 		ck_hash_password = ck_hash.hexdigest()
 
 		return self._password == ck_hash_password
+
+	def to_dict(self):
+		return self.e_d
